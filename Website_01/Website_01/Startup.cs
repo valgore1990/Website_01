f1e4395bbcca8a3070fcb49949adab47f1d3a43a
Josh Brown <Josh Brown>
2017-02-26 17:50:17 -0800

    first commit

---
+++ Website_01/Website_01/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Website_01.Startup))]
+namespace Website_01
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
